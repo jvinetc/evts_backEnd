7f45e11d4029ca23f08655894cd0151f3796f0b1
Jose Vinet <jvinetc@gmail.com>
2025-08-06 21:43:15 -0400

    probando git

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,7 +1,3 @@
-export const eliminarTildes=(texto:string)=> {
-    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g,"");
-}
-
 export const formatResponse=(succes:boolean, data:object, message:string, error:boolean)=>{
     return{
         succes,
